76fc8886a4a1be526240fbd6105753cc96bf6d46
Anthony Jean <anthony.jean@netc.eu>
2019-01-29 15:57:35 +0100

    Tinder-like swipe with UIView

---
+++ Apple Logo Game/MyButton.swift
@@ -0,0 +1,21 @@
+//
+//  MyButton.swift
+//  Apple Logo Game
+//
+//  Created by Anthony Jean on 29/01/2019.
+//  Copyright © 2019 Anthony Jean. All rights reserved.
+//
+
+import UIKit
+
+class MyButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Apple Logo Game/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  Apple Logo Game
+//
+//  Created by Anthony Jean on 29/01/2019.
+//  Copyright © 2019 Anthony Jean. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
